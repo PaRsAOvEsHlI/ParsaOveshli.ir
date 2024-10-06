180946c6c49c21295260a971387619617a7ad18f
ParsaOveshli <104127485+PaRsAOvEsHlI@users.noreply.github.com>
2024-10-06 08:27:41 -0700

    Delete Amat directory

--- Amat/app.js
+++
@@ -1,6 +0,0 @@
-let root = document.documentElement,
-    checkbox = document.getElementById("darkmode-input");
- 
-checkbox.addEventListener("input", function() {
-    root.classList.toggle("dark");
-})
